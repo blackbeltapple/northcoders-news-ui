e16986b7e7521a339c66014bd5d58a3e9c4651e0
amanda manekshaw <amanda_manekshaw@hotmail.com>
2016-11-24 16:25:59 +0000

    Implemented GET call to fetch topics and render them in topicbar

--- src/components/TopicBar.js
+++ src/components/TopicBar.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import {Link} from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from '../actions/actions';
 
-const App = React.createClass({
+
+
+const TopicBar = React.createClass({
+  componentWillMount: function () {
+    this.props.fetchTopics();
+  },
+
+  renderTopics: function () {
+    return (
+        this.props.topics.map( function (topic) {
+          return (
+            <Link to={`/topics/${topic.title}/articles`} >{topic.title}</Link>
+          )
+        })
+    )
+  },
+
   render: function () {
+    console.log('this.props.topics', this.props.topics);
     return (
       <div>
-        <h3>TopicBar</h3>
-        <Link to='/topics/cooking/articles'>Cooking</Link>
-        <Link to='/topics/coding/articles'>Coding</Link>
-        <Link to='/topics/football/articles'>Football</Link>
+        {this.renderTopics()}
       </div>
     );
   }
 });
 
-export default App;
+function mapStateToProps (state) {
+  return {
+    topics: state.topics
+  };
+}
+
+function mapDispatchToProps (dispatch, props) {
+  return {
+    fetchTopics: () => {
+      dispatch(actions.fetchTopics(props));
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopicBar);
