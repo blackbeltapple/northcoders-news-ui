9fe59f85a33709e21c215eed5d5bb5215bfe89fa
Tague Ferguson <tagueferguson@live.co.uk>
2016-11-25 11:23:15 +0000

    Updates the Article to include the store, and present information from a specific article

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -13,8 +13,8 @@ actions.fetchArticles = function () {
         if (err) dispatch({ type: types.FETCH_ARTICLES_ERROR, err });
         else dispatch({ type: types.FETCH_ARTICLES_SUCCESS, data: res.body });
       });
-  }
-}
+  };
+};
 
 actions.fetchTopics = function () {
   return (dispatch) => {
@@ -25,49 +25,79 @@ actions.fetchTopics = function () {
         if (err) dispatch({ type: types.FETCH_TOPICS_ERROR, err });
         else dispatch({ type: types.FETCH_TOPICS_SUCCESS, data: res.body });
       });
-  }
-}
+  };
+};
+
+actions.fetchComments = function (id) {
+  let articleId = id;
+  return (dispatch) => {
+    dispatch({ type: types.FETCH_COMMENTS_REQUEST });
+    request
+      .get(`${ROOT}/articles/${articleId}/comments`)
+      .end((err, res) => {
+        if (err) dispatch({ type: types.FETCH_TOPICS_ERROR, err });
+        else dispatch({ type: types.FETCH_COMMENTS_SUCCESS, data: res.body });
+      });
+  };
+};
 
 actions.fetchArticleSuccess = function (data) {
   return {
     type: types.FETCH_ARTICLES_SUCCESS,
     data: data
-  }
-}
+  };
+};
 actions.fetchArticleRequest = function () {
   return {
     type: types.FETCH_ARTICLES_SUCCESS
-  }
-}
+  };
+};
 actions.fetchArticleError = function (error) {
   return {
     type: types.FETCH_ARTICLES_SUCCESS,
     error: error
-  }
-}
+  };
+};
 actions.fetchTopicsSuccess = function (data) {
   return {
     type: types.FETCH_TOPICS_SUCCESS,
     data: data
-  }
-}
+  };
+};
 actions.fetchTopicsRequest = function () {
   return {
     type: types.FETCH_TOPICS_REQUEST
-  }
-}
+  };
+};
 actions.fetchTopicsError = function (error) {
   return {
     type: types.FETCH_TOPICS_ERROR,
     error: error
-  }
-}
+  };
+};
 
+actions.fetchCommentsSuccess = function (data) {
+  return {
+    type: types.FETCH_ARTICLE_SUCCESS,
+    data: data
+  };
+};
+actions.fetchCommentsRequest = function () {
+  return {
+    type: types.FETCH_ARTICLE_REQUEST
+  };
+};
+actions.fetchCommentsError = function (error) {
+  return {
+    type: types.FETCH_ARTICLE_ERROR,
+    error: error
+  };
+};
 actions.setFilter = function (filter) {
   return {
     type: types.SET_FILTER,
     filter: filter
-  }
-}
+  };
+};
 
 export default actions;
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,55 @@
 import React from 'react';
 import CommentList from './CommentList';
+import {connect} from 'react-redux';
+import actions from '../actions/actions';
+import VoteBox from './VoteBox'
 
 const Article = React.createClass({
+  componentWillMount: function () {
+    this.props.fetchArticles();
+  },
+  findArticle: function () {
+    if(this.props.loading === true) {
+      return "Hell00000000000000";
+    }
+    let find = this.props.articles.filter((article) => {
+      return article._id === this.props.params.article;
+    });
+    return find;
+  },
   render: function () {
+    let article = this.findArticle()[0];
+
     return (
-      <div>
-        <h3 className='title is-3'>ARTICLE Number: {this.props.params.article}</h3>
+      <div className='box'>
+
+        <h1 className='title is-2'>{article.title}</h1>
+        <p className='title is-4'>{article.body}</p>
+        <p className='title is-5'>Created by: {article.created_by}</p>
+        <p className='title is-5'>Comments: {article.comments}</p>
+        <VoteBox />
         <CommentList />
       </div>
     );
   }
 });
 
-export default Article;
+function mapStateToProps (state) {
+  return {
+    loading: state.loading,
+    articles: state.articles
+  };
+}
+
+function mapDispatchToProps (dispatch, props) {
+  return {
+    fetchArticles: () => {
+      dispatch(actions.fetchArticles());
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Article);
+
+
+ // {this.props.params.article}
