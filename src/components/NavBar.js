a7a9ba840821dadcb531544bb52a1416e9f55395
amanda manekshaw <amanda_manekshaw@hotmail.com>
2016-11-27 22:15:14 +0000

    Styled NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,44 +3,44 @@ import {Link} from 'react-router';
 
 const NavBar = React.createClass({
   render: function () {
-    console.log('NavBar render')
+    console.log('NavBar render');
     return (
       <div>
-        <nav className="nav">
-          <div className="nav-left">
-            <figure className="image is-128x128">
-              <img src="http://www.thesharpproject.co.uk/assets/Northcoders_gallery.png" alt="Northcoders"  />
+        <nav className='nav'>
+          <div className='nav-left'>
+            <figure className='image is-128x128'>
+              <img src='http://www.thesharpproject.co.uk/assets/Northcoders_gallery.png' alt='Northcoders' />
             </figure>
           </div>
-          <div className="nav-center">
-            <a className="nav-item" href="#">
-              <span className="icon">
-                <i className="fa fa-github"></i>
+          <div className='nav-center'>
+            <a className='nav-item' href='#'>
+              <span className='icon'>
+                <i className='fa fa-github' />
               </span>
             </a>
-            <a className="nav-item" href="#">
-              <span className="icon">
-                <i className="fa fa-twitter"></i>
+            <a className='nav-item' href='#'>
+              <span className='icon'>
+                <i className='fa fa-twitter' />
               </span>
             </a>
           </div>
-          <div className="nav-right nav-menu">
-            <a className="nav-item" href="#">
+          <div className='nav-right nav-menu'>
+            <a className='nav-item' href='#'>
               Home
             </a>
-            <a className="nav-item" href="#">
+            <a className='nav-item' href='#'>
               Documentation
             </a>
-            <a className="nav-item" href="#">
+            <a className='nav-item' href='#'>
               Blog
             </a>
           </div>
         </nav>
-        <nav className="nav has-shadow">
-          <div className="container">
-            <div className="nav-left">
-              <a className="nav-item is-tab is-active"><Link to='/'>Articles</Link></a>
-              <a className="nav-item is-tab"><Link to='/userlist'>Users</Link></a>
+        <nav className='nav has-shadow'>
+          <div className='container'>
+            <div className='nav-left'>
+              <a className='nav-item is-tab is-active'><Link to='/'>Articles</Link></a>
+              <a className='nav-item is-tab'><Link to='/userlist'>Users</Link></a>
             </div>
           </div>
         </nav>
