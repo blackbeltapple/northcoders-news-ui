48dc173412a23c45534e3f8df5937c952ba1ed55
amanda manekshaw <amanda_manekshaw@hotmail.com>
2016-11-27 21:46:33 +0000

    Styled NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,21 +5,46 @@ const NavBar = React.createClass({
   render: function () {
     console.log('NavBar render')
     return (
-      // <div>
-      //   <Link to='/'>Articles</Link>
-      //   <Link to='/userlist'>Users</Link>
-      // </div>
-      //
-
-      <nav className="nav has-shadow">
-        <div className="container">
+      <div>
+        <nav className="nav">
           <div className="nav-left">
-            <a className="nav-item is-tab is-active"><Link to='/'>Articles</Link></a>
-            <a className="nav-item is-tab"><Link to='/userlist'>Users</Link></a>
+            <figure className="image is-128x128">
+              <img src="http://www.thesharpproject.co.uk/assets/Northcoders_gallery.png" alt="Northcoders"  />
+            </figure>
           </div>
-        </div>
-      </nav>
-
+          <div className="nav-center">
+            <a className="nav-item" href="#">
+              <span className="icon">
+                <i className="fa fa-github"></i>
+              </span>
+            </a>
+            <a className="nav-item" href="#">
+              <span className="icon">
+                <i className="fa fa-twitter"></i>
+              </span>
+            </a>
+          </div>
+          <div className="nav-right nav-menu">
+            <a className="nav-item" href="#">
+              Home
+            </a>
+            <a className="nav-item" href="#">
+              Documentation
+            </a>
+            <a className="nav-item" href="#">
+              Blog
+            </a>
+          </div>
+        </nav>
+        <nav className="nav has-shadow">
+          <div className="container">
+            <div className="nav-left">
+              <a className="nav-item is-tab is-active"><Link to='/'>Articles</Link></a>
+              <a className="nav-item is-tab"><Link to='/userlist'>Users</Link></a>
+            </div>
+          </div>
+        </nav>
+      </div>
     );
   }
 });
